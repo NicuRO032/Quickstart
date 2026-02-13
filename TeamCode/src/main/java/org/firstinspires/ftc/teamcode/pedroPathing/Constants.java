de55cc5a378201c99a554fe77319693900c35127
RO032 IDeaL Electronics Cugir <ideal@idl-cugir.ro>
2026-02-13 08:03:49 +0200

    Vineri dimineata inainte de Medias
    pentru sincronizare githib

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,6 +7,7 @@ import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -15,13 +16,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(10.6)
-            .forwardZeroPowerAcceleration(-44.03145581707733)
-            .lateralZeroPowerAcceleration(-60.6283102361945)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.03, 0, 0.0004, 0.001))
-            .headingPIDFCoefficients(new PIDFCoefficients(0.9, 0, 0.02, 0.01))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.009, 0, 0.0005, 0.6, 0.01))
-            .centripetalScaling(0.005);
+            .mass(13.8)
+            .forwardZeroPowerAcceleration(-39.68006229544917)
+            .lateralZeroPowerAcceleration(-75.90603183464327)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.005, 0.015))
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.05, 0.03))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.02,0.0,0.0001,0.6,0.001))
+            .centripetalScaling(0.00057)
+            ;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -33,38 +35,34 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(56.37572643643927)
-            .yVelocity(45.100581114984);
+            .xVelocity(58.9961357680407)
+            .yVelocity(49.07686353560646)
+            ;
 
-    public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(.0020047656469968)
-            .strafeTicksToInches(.0018042890822971)
-            .turnTicksToInches(.0017564914038382)
-            .leftPodY(2.95)
-            .rightPodY(-2.95)
-            .strafePodX(-3.75)
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .leftPodY(3.14)
+            .rightPodY(-3.14)
+            .strafePodX(-4.52)
+            .forwardTicksToInches(.001948677514836943)
+            .strafeTicksToInches(.0019876010767738263)
+            .turnTicksToInches(.0018844454715644498)
             .leftEncoder_HardwareMapName("lr")
             .rightEncoder_HardwareMapName("rf")
             .strafeEncoder_HardwareMapName("rr")
             .leftEncoderDirection(Encoder.FORWARD)
             .rightEncoderDirection(Encoder.FORWARD)
             .strafeEncoderDirection(Encoder.FORWARD)
-            .IMU_HardwareMapName("imu")
-            .IMU_Orientation(
-                    new RevHubOrientationOnRobot(
-                            RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                            RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
-                    )
-            );
+            ;
 
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1.3, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(1.5, 100, 1, 1);
+
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
-                .threeWheelIMULocalizer(localizerConstants)
+                .threeWheelLocalizer(localizerConstants)
                 .build();
     }
 }
